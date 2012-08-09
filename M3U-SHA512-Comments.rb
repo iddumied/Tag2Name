8034365e020535aec9744062d910793c3868d6fc
Master Chief <extra07@online.de>
2012-08-09 14:39:48 +0200

    working

--- M3U-SHA512-Comments.rb
+++ M3U-SHA512-Comments.rb
@@ -1,4 +1,6 @@
 require 'digest'
+require './Dir.rb'
+require './SHA512-Database.rb'
 
 ##
 # Just a little script to add SHA512 summs to a playlist, and restore
@@ -26,18 +28,26 @@ class M3U
     end
 
     def to_s
-      "#{ @comments }#SHA512:#{ @sha512 }\n#{ @file }"
+      str =  "#{ @comments }"
+      str += "#SHA512:#{ @sha512 }" unless @sha512 == ""
+      str += "\n" if @sha512 != "" and @file != ""
+      str += "#{ @file }" unless @file == ""
+      str
     end
     alias inspect to_s
   end
 
+
   def initialize file
+
+    @file = file
     
     @log = File.open("M3U-SHA512-Log", "a")
+    @log.puts "\n\n" + "="*100
     @log.puts "Time: #{ Time.now }\nFile: #{ file }"
 
     # cd to the playlist dir
-    Dir.chdir file.split("/").map { |e| e + "/" unless e == file.split("/").last }.join
+    Dir.chdir file.split("/").map { |e| e + "/" unless e == file.split("/").last }.join if file.include? "/"
 
     @entries = []
     @entries << Entry.new
@@ -48,8 +58,8 @@ class M3U
 
       if line.start_with? "#SHA512:"
         sha512 = line.gsub("#SHA512:","")
-        sha512 = "" unless sha.length == 128
-        @entries.last.add_sha512 sha12
+        sha512 = "" unless sha512.length == 128
+        @entries.last.add_sha512 sha512
       
       elsif line.start_with? "#"
         @entries.last.add_comment line
@@ -74,7 +84,10 @@ class M3U
           puts "[EE] #{ e }: #{ line }" if $VERBOSE
         end
 
-        @entries.last.add_file file
+        @log.puts "[II] m3u entry: #{ line }"
+        puts "[II] m3u entry: #{ line }" if $VERBOSE
+
+        @entries.last.add_file line
         @entries << Entry.new
       end
     end
@@ -82,32 +95,102 @@ class M3U
     file.close
   end
 
+  ##
+  # restores an m3u file based on the SHA512 SUMs
+  #
+  def restore path_to_database
+    path_to_database.sub Dir.pwd, ""
+    path_to_database.sub "/", ""
+    
+    @database ||= Database.new(path_to_database, @log)
+    @entries.each do |entry|
+      unless @database[entry.sha512].nil?
+        oldfile = entry.file
+        entry.add_file @database[entry.sha512]
+        @log.puts "[II] restored #{ oldfile } => #{ entry.file }"
+        puts "[II] restored #{ oldfile } => #{ entry.file }" if $VERBOSE
+      else
+        @log.puts "[WW] couldn't find and restore #{ entry.file } in Database"
+        puts "[WW] couldn't find and restore #{ entry.file } in Database" if $VERBOSE
+      end
+    end
+  end
+
+  def save_database name
+    unless @database.nil?
+      @database.save name
+    else
+      @log.puts "[EE] couldn't save Database, Database didn't exists"
+      puts "[EE] couldn't save Database, Database didn't exists" if $VERBOSE
+    end      
+  end
+
+
   def to_s
     str = ""
     @entries.each { |e| str += e.to_s + "\n" }
-    str
+    str.chop
   end
   alias inspect to_s
+
+  def save
+    begin
+      file = File.new(@file, "w")
+      file.write to_s
+      file.close
+
+      @log.puts "[II] SHA512 SUMs written to file #{ @file }"
+      puts "[II] SHA512 SUMs written to file #{ @file }" if $VERBOSE
+
+    rescue Exception => e
+      @log.puts "[EE] #{ e }: while writting file#{ @file }"
+      puts "[EE] #{ e }: while writting file#{ @file }" if $VERBOSE
+    end
+  end
 
 end
 
 if __FILE__ == $0
   require 'slop'
 
-  require slop
-
   opts = Slop.new do
     banner "ruby #{ $0 } [options]"
     help 
     on :t, :target,  "Target file", :argument => true
+    on :v, :verbose, "Be Verbose"
+    on :c, :create,  "Create SHA512 SUMs"
+    on :p, :print,   "Print Output (can be used without create to show changes)"
+    on "--database-source", "Path to Database Source dir, absolute or relative to target", :argument => true
+    on :database, "Database file", :argument => true
+    on "--create-database", "Create the database from database-source" 
+    on "--database-target", "File to save the Database", :argument => true
+    on "--restore", "restore m3u file using database"
   end
 
   opts.parse
+  $VERBOSE = true if opts.verbose?
+
 
   unless opts.target?
     puts opts
     exit
   end
+  
+  m3u = M3U.new opts[:target]
 
-  puts M3U.new(opts[:target]).to_s
+  if opts.restore?
+    if (opts["database-source"].nil? and opts["database"].nil?) or
+          (opts["create-database"] != nil and opts["database-target"].nil?)
+      puts opts
+      exit
+    end
+
+    m3u.restore opts["database"] unless opts["database"].nil?
+    m3u.restore opts["database-source"] unless opts["database-source"].nil?
+
+    m3u.save_database opts["database-target"] unless opts["create-database"].nil?
+  end
+
+  m3u.save if opts.create?
+  puts m3u.to_s if opts.print?
 end
---
+++ SHA512-Database.rb
@@ -0,0 +1,100 @@
+require './Dir.rb'
+require 'digest'
+
+##
+# implementation of an Database which saves the sha512 sum for files
+#
+class Database
+  def initialize path, log, blocksize = 1024**2 * 100 
+    @log = log
+    @database = { }
+    @blocksize = blocksize
+
+    begin
+      if File.directory? path
+        Dir.recursive(path) do |file|
+          begin
+            @database.store sha512sum(file), file
+            @log.puts "[II] database entry: #{ file }"
+            puts "[II] database entry: #{ file }" if $VERBOSE
+          rescue Exception => e
+            @log.puts "[EE] #{ e } by adding entry to Database"
+            puts "[EE] #{ e } by adding entry to Database" if $VERBOSE
+          end
+        end
+     
+      elsif File.exists? path
+        File.open(path).each_line do |line|
+          line.chop!
+          @database.store line[0,128], line[128, line.length - 128]
+        end
+        @log.puts "[II] Database readed #{ @database.length } entries found in: #{ path }"
+        puts "[II] Database readed #{ @database.length } entries found in: #{ path }" if $VERBOSE
+     
+      else
+        @log.puts "[EE] no such fil or directory: #{ path }"
+        puts "[EE] no such fil or directory: #{ path }" if $VERBOSE
+      end
+    rescue Exception => e
+      @log.puts "[EE] #{ e } while creating Database from #{ path }"
+      puts "[EE] #{ e } while creating Database from #{ path }" if $VERBOSE
+      @log.close
+      raise e
+    end
+  end
+
+  def [] index; @database[index]; end
+  def []= index, var; @database[index] = var; end
+
+  def save fname
+    begin
+      file = File.new(fname, "w")
+      @database.each { |k, v| file.puts(k + v) }
+      file.close
+      @log.puts "[II] #{ @database.length } Database enries writtten to #{ fname }"
+      @log.puts "[II] #{ @database.length } Database enries writtten to #{ fname }" 
+    rescue Exception => e
+      @log.puts "[EE] #{ e } while saving Database to file #{ fname }"
+      puts "[EE] #{ e } while saving Database to file #{ fname }" if $VERBOSE
+    end
+  end
+
+  private
+
+  def sha512sum file
+    File.open(file, 'rb') do |io|
+      dig = Digest::SHA512.new
+      while (buf = io.read(@blocksize))
+        dig.update(buf)
+      end
+      dig.hexdigest
+    end
+  end
+
+end
+
+if __FILE__ == $0
+  require 'slop'
+
+  opts = Slop.new do
+    banner "ruby #{ $0 } [options]"
+    help 
+    on :s, :source,  "Source file", :argument => true
+    on :t, :target,  "Target file", :argument => true
+    on :v, :verbose, "Be Verbose"
+  end
+
+  opts.parse
+  $VERBOSE = true if opts.verbose?
+
+  unless opts.target? and opts.source?
+    puts opts
+    exit
+  end
+  
+  log = File.open "DatabaseLog", "a"
+  log.puts "\n\n" + "="*100
+  log.puts "Time: #{ Time.now }\n\n"
+  Database.new(opts[:source], log).save opts[:target]
+
+end
